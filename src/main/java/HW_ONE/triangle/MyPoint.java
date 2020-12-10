20ee87f908e20303e17650304d86cf7392789713
Disteonne <diana.razmikovna@yandex.ru>
2020-12-11 01:08:36 +0300

    hw part1

--- src/main/java/HW_ONE/triangle/MyPoint.java
+++ src/main/java/HW_ONE/triangle/MyPoint.java
@@ -4,6 +4,14 @@ public class MyPoint {
     private int x;
     private int y;
 
+    public MyPoint() {
+    }
+
+    public MyPoint(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
     public int getX() {
         return x;
     }
